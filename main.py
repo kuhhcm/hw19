fd4ce2b11af7a4da06c333065d64d0c548b0a268
kuhhcm <158571357+kuhhcm@users.noreply.github.com>
2024-03-17 23:28:36 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+# # Задание 1
+# def create_file(file):
+#     time.sleep(1)
+#     with open(file, 'w') as file:
+#         file.write('')
+        
+# create_file('file.txt')
+
+# # Задание 2
+# def create_file(file):
+#     time.sleep(1)
+#     with open(file, 'w') as file:
+#         file.write('')
+        
+# start = time.time()
+# for i in range(1,101):
+#     create_file(f'file{i}.txt')
+# end = time.time()
+# print("Время : ", end - start)
